88bdd6746e378ddbd6b58979bd18ad703c9390b1
root <root@WS06.localdomain>
2021-10-21 16:25:27 +0300

    clean some

---
+++ rest/add2dict.sh
@@ -0,0 +1,5 @@
+until false
+do
+    read -p "Type string: " strtodict
+    echo "$strtodict" >> new_words.txt 
+done
---
+++ rest/words2col_rest.py
@@ -0,0 +1,84 @@
+import os, sys
+from functools import reduce
+
+filepath = "new_words.txt"
+
+
+#def make_table(grid):
+#    cell_width = 2 + max(
+#        reduce(lambda x, y: x + y, [[len(item) for item in row] for row in grid], [])
+#    )
+#    num_cols = len(grid[0])
+#    rst = table_div(num_cols, cell_width, 0)
+#    header_flag = 1
+#    for row in grid:
+#        rst = (
+#            rst
+#            + "| "
+#            + "| ".join([normalize_cell(x, cell_width - 1) for x in row])
+#            + "|\n"
+#        )
+#        rst = rst + table_div(num_cols, cell_width, header_flag)
+#        header_flag = 0
+#    return rst
+
+
+def table_div(num_cols, col_width, header_flag):
+    if header_flag == 1:
+        return num_cols * ("+" + (col_width) * "=") + "+"
+    else:
+        return num_cols * ("+" + (col_width) * "-") + "+"
+
+
+def normalize_cell(string, length):
+    return string + ((length - len(string)) * " ")
+
+
+def file_ex(file):
+    """If file exists, you can divide each row to words in cells."""
+    if not os.path.isfile(file):
+        print(f"File path {filepath} does not exist. Exiting...")
+        sys.exit()
+
+
+def str2cell(line):
+    res = ""
+    iter_w = map(lambda word: f"| {word.ljust(19)}|", line)
+    try:
+        pos = next(iter_w)
+    except:
+        return
+    while True:
+        try:
+            _pos = next(iter_w)
+            if len(line) < 3:
+                res += pos + "".ljust(20)
+            else:
+                pos = pos[:-1]
+                res += pos
+            pos = _pos
+        except StopIteration:
+            res += pos
+            break
+    return res
+
+
+def split_file_strings(header=True, sep=" "):
+    file_ex(filepath)
+
+    def p_inline(line, sep, header):
+        print(str2cell(line.strip().split(sep)))
+        print(table_div(3, 20, header))
+
+    with open(filepath, "r") as f:
+        print(table_div(3, 20, 0))
+        if header:
+            p_inline(f.readline(), sep, header)
+            header = False
+        for _line in f:
+            p_inline(_line, sep, header)
+
+
+if __name__ == "__main__":
+
+    split_file_strings()
