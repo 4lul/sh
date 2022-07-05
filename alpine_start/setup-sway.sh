9682c0888a2a3a2f593ca25a10c6356c8b2792b9
4lul <91326095+4lul@users.noreply.github.com>
2022-07-05 14:10:26 +0300

    Update setup-sway.sh

--- alpine_start/setup-sway.sh
+++ alpine_start/setup-sway.sh
@@ -2,13 +2,15 @@ apk add dbus-x11 eudev && \
 setup-udev && \
 apk add mesa-dri-gallium && \
 setup-xorg-base && \
-apk add xf86-video-ati && \
-echo fbcon >> /etc/modules && \
-echo radeon >> /etc/modules && \
+#apk add xf86-video-ati && \
+#echo fbcon >> /etc/modules && \
+#echo radeon >> /etc/modules && \
 apk add mkinitfs && \
 echo features=\"keymap cryptsetup kms ata base ide scsi usb virtio ext4\" >> /etc/mkinitfs/mkinitfs.conf
 mkinitfs
-printf 'press [ENTER] to continue deleting...'
+read -p "Enter username: " USER
+printf $USER
+#printf 'press [ENTER] to continue deleting...'
 read _
 adduser $USER input
 adduser $USER video
@@ -16,5 +18,15 @@ apk add ttf-dejavu && \
 apk add seatd && \
 rc-update add seatd && \
 rc-service seatd start && \
-adduser $USER seat
-
+adduser $USER seat && \
+apk add seatd-launch && \
+apk add elogind polkit-elogind && \
+rc-update add elogind && \
+rc-service elogind start && \
+apk add sway sway-doc && \
+apk add xwayland foot dmenu swaylock swaybg swayidle && \
+mkdir ~/.run
+#XDG_RUNTIME_DIR=~/.run dbus-launch sway
+#XDG_RUNTIME_DIR=~/.run dbus-launch seatd-launch sway
+#alias swayinit="XDG_RUNTIME_DIR=~/.run dbus-launch seatd-launch sway"
+#swaylock needs to be able to read your /etc/shadow file to be able to validate
